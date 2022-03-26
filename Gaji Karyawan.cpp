d9cc8248bb1fc138d30097ca75b40d69234fecfa
A063MuhammadIrfanAryaSyahputra <101864606+A063MuhammadIrfanAryaSyahputra@users.noreply.github.com>
2022-03-26 09:49:01 +0700

    Update Gaji Karyawan.cpp

--- Gaji Karyawan.cpp
+++ Gaji Karyawan.cpp
@@ -1,67 +1,64 @@
 #include<iostream>
 using namespace std;
 
-class proses{
+class process{
 	public :
-		int bulan;
-		float total;
-		float tambahan;
+		int bul;
+		float tot, tbh;
 		
 		void input(int a,string tim);
 		void output(string tim);
-		
-}pro;
+}prs;
 
-void proses::input(int a,string tim){
+void process::input(int a,string tim){
 	
 	cout<<endl;
 	cout<<"Masukan Jumlah Bulan Terselesaikannya Tugas : ";
-	cin>>pro.bulan;
+	cin>>prs.bul;
 	
-	if(pro.bulan<=5){
-		pro.total=a*pro.bulan;
-		pro.tambahan=pro.total*0.06;
+	if(prs.bul!=5){
+		prs.tot=a*prs.bul;
+		prs.tbh=prs.tot*0.06;
 		
 	}
 	else{
-		pro.total=a*pro.bulan;
+		prs.tot=a*prs.bul;
 		
 	}
-	pro.output(tim);
+	prs.output(tim);
 	
 }
 
-void proses::output(string tim){
+void process::output(string tim){
 	system("cls");
-	cout<<"========================================"		<<endl;
-	cout<<"           SLIP GAJI ANDA               "		<<endl;
-	cout<<"========================================"		<<endl;
-	cout<<"									"				<<endl;
-	cout<<"	Developer : " <<tim								<<endl;
-	cout<<"	Jumlah    : " <<pro.bulan<<" Bulan"				<<endl;
-	cout<<" 	Tambahan  : " <<pro.tambahan<<" Juta"			<<endl;
-	cout<<"	Gaji 	  : " <<pro.total+pro.tambahan<<" Juta" <<endl;
-	cout<<"========================================"		<<endl;
+	cout<<"||========================================||"    <<endl;
+	cout<<"                SLIP GAJI                "		<<endl;
+	cout<<"||========================================||"    <<endl;
+	cout<<"||Developer : " <<tim<<"                        ||"				    <<endl;
+	cout<<"||Jumlah    : " <<prs.bul<<" Bulan                     ||"				<<endl;
+	cout<<"||Tambahan  : " <<prs.tbh<<" Juta                   ||"	            <<endl;
+	cout<<"||Gaji 	    : " <<prs.tot+prs.tbh<<" Juta                  ||"       <<endl;
+	cout<<"||========================================||"    <<endl;
 		
 }
 
 int main(){
 	int pilih;
-	cout<<"========================================"		<<endl;
-	cout<<"        PROGRAM GAJI DEVELOPER          "		<<endl;
-	cout<<"========================================"		<<endl;
-	cout<<" 									   "		<<endl;
-	cout<<" TIM 1 22 JUTA  						   "		<<endl;
-	cout<<" TIM 2 28 JUTA						   "		<<endl;
-	cout<<" 									   "		<<endl;
-	cout<<"========================================"		<<endl;
+	cout<<"||========================================||"		<<endl;
+	cout<<"           PROGRAM GAJI + BONUS             "		<<endl;
+	cout<<"||========================================||"		<<endl;
+	cout<<"||            TEAM 1 22 JUTA              ||"		<<endl;
+	cout<<"||            TEAM 2 28 JUTA	          ||"		<<endl;
+	cout<<"||   Bonus 6% dari gaji pokok jika bisa   ||"		<<endl;
+	cout<<"||   menyelesaikan kurang dari 5 bulan    ||"		<<endl;
+	cout<<"||========================================||"		<<endl;
 	
-	cout<<"Masukan Pilihan Anda : ";
+	cout<<"Masukan Team  : ";
 	cin>>pilih;
 	if(pilih==1){
-		pro.input(22,"TIM1");
+		prs.input(22,"TIM1");
 	}
 	else(pilih==2);{
-		pro.input(28,"TIM2");
+		prs.input(28,"TIM2");
 	}
 }
